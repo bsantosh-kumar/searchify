cc4eff2c39ee2fc0c951677de60c0d17aa86cb81
Vishwas <vishwasgajawada2002@gmail.com>
2022-03-26 11:07:01 +0530

    switch file types

--- searchify_django/app/views.py
+++ searchify_django/app/views.py
@@ -5,27 +5,50 @@ from django.views.generic import FormView
 from .forms import *
 
 import pytesseract    # ======= > Add
-try:
-    from PIL import Image
-except:
-    import Image
+from PIL import Image
 
 
 def index(request) :
     return HttpResponse("Hello, world. You're at the index.")
 
+def extractWordsFromImage(image) :
+    txt = pytesseract.image_to_string(Image.open(image))
+    return txt
+
+def split_string(txt) :
+    import re
+    txt = txt.lower()
+    txt = re.sub(r'[^\w\s]',' ',txt)
+    txt = re.sub(r'\s+',' ',txt)
+    return txt.split()
+    
 class HomeView(FormView):
     form_class = UploadForm
     template_name = 'app/index.html'
     success_url = '/'
-
-    # def form_valid(self, form):
-    #     upload = self.request.FILES['file']
-    #     return super().form_valid(form)
     
     def form_valid(self, form):
         upload = self.request.FILES['file']
-        txt = pytesseract.image_to_string(Image.open(upload))
         print("upload = ",upload)
-        print(type(txt), txt) # =====> add line
+        file_type = upload.content_type
+        print("file_type = ",file_type)
+        
+        words = []
+
+        if(file_type.endswith(('jpeg','png'))):
+            # txt = extractWordsFromImage(upload)
+            # words = split_string(txt)
+            # print(words)
+            print('image')
+        else :
+            print("not image")
+        # if(file_type == 'image/jpeg' or file_type == 'image/png' or file_type == 'image/jpg'):
+        #     txt = extractWordsFromImage(upload)
+        #     words = split_string(txt)
+        #     print(words)
+        # else :
+        #     print("not image")
+
+        # remove all stop words after extracting from any kind of file
+        
         return super().form_valid(form)
