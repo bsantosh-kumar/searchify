e086081e7c1b5fa1c9564ae84d6475399219ef63
Vishwas <vishwasgajawada2002@gmail.com>
2022-03-26 09:56:51 +0530

    extracting words from an image

--- searchify_django/app/urls.py
+++ searchify_django/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.HomeView.as_view(), name='index'),
 ]
--- searchify_django/app/views.py
+++ searchify_django/app/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.http import JsonResponse
+from django.views.generic import FormView
+from .forms import *
+
+import pytesseract    # ======= > Add
+try:
+    from PIL import Image
+except:
+    import Image
+
+
 def index(request) :
     return HttpResponse("Hello, world. You're at the index.")
+
+class HomeView(FormView):
+    form_class = UploadForm
+    template_name = 'app/index.html'
+    success_url = '/'
+
+    # def form_valid(self, form):
+    #     upload = self.request.FILES['file']
+    #     return super().form_valid(form)
+    
+    def form_valid(self, form):
+        upload = self.request.FILES['file']
+        txt = pytesseract.image_to_string(Image.open(upload))
+        print("upload = ",upload)
+        print(type(txt), txt) # =====> add line
+        return super().form_valid(form)
